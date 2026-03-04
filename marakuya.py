812dbe1244dcb493f501ab01566aa537aee51076
Android31-coder <andriudeuneka@gmail.com>
2026-03-04 19:03:35 +0200

    /

--- marakuya.py
+++ marakuya.py
@@ -4,12 +4,39 @@ from kivymd.uix.screen import MDScreen
 
 from kivy import platform
 from kivy.core.window import Window
+from kivy.clock import Clock
+from kivy.metrics import dp, sp
+
+FPS = 60
+SHIP_SPEED = dp(5)
 
 class MainScreen(MDScreen):
     pass
 
 class GameScreen(MDScreen):
-    pass
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        Clock.schedule_interval(self.update, 1/FPS)
+        self.eventkeys = {}
+    def update(self, dt):
+        for key in self.eventkeys:
+            if self.eventkeys[key] == True:
+                if key == "left":
+                    self.moveLeft()
+                if key == "right":
+                    self.moveRight()
+
+    def pressKey(self, key):
+        self.eventkeys[key] = True
+
+    def releaseKey(self, key):
+        self.eventkeys[key] = False
+
+    def moveLeft(self):
+        self.ids.ship.pos[0] -= SHIP_SPEED
+
+    def moveRight(self):
+        self.ids.ship.pos[0] += SHIP_SPEED
 
 class ShooterApp(MDApp):
     def build(self):
